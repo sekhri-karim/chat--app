a2011378759f7657a9717a4f4ce649b00bb14e98
karim <karim@gmail.com>
2024-06-09 13:41:49 +0100

    uploaded the full project

---
+++ src/main/java/com/mycompany/mvc/Mvc.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package com.mycompany.mvc;
+
+public class Mvc {
+
+    public static void main(String[] args) {
+        // Create an instance of the Main frame
+        main mainFrame = new main();
+        mainFrame.setVisible(true);
+        export exp =new export();
+        exp.setVisible(true);
+        chat chat =new chat() ;
+        chat.setVisible(true);
+    }
+}
